bc38e5b93dca35566af3b1c389a057ab1a788143
maikilo <maikilo>
2025-05-17 16:13:49 +0300

    add database connection to api config

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import _ "github.com/lib/pq"
 
 import (
+	"os"
+	"github.com/joho/godotenv"
+	"database/sql"
+	"github.com/maikilo/chirpy/internal/database"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -10,14 +14,24 @@ import (
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries *database.Queries
 }
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("error opening db connection %v", err)
+	}
 
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries : database.New(db),
 	}
 
 	mux := http.NewServeMux()
