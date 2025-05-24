bf16cf1e0e9e6efa52b4ecc11923849129e2a865
maikilo <maija.loyska@gmail.com>
2025-05-24 18:59:47 +0300

    edits to schemas

--- sql/schema/002_chirps.sql
+++ sql/schema/002_chirps.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 CREATE TABLE chirps (
-	id UUID PRIMARY KEY,
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
-	body TEXT NOT NULL UNIQUE,
-    user_id UUID NOT NULL REFERENCES users (user_id) ON DELETE CASCADE
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    body TEXT NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- +goose Down
